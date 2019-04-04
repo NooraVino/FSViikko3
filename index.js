75b281752693d0091d1a4fffeaaeb0b8fa566667
Noora Virolainen <vino@cubbli18-gold.local>
2019-04-04 10:49:56 +0300

    3.2-3.4 info ja yksittäinen muistiinpano ja sen poistaminen

--- index.js
+++ index.js
@@ -28,6 +28,30 @@ let persons = [
     res.json(persons)
   })
 
+  app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(p => p.id === id)
+    
+    if (person) {
+      response.json(person)
+    } else {
+      response.status(404).end()
+    }
+  })
+
+  app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(p => p.id !== id)
+
+    response.status(204).end()
+  })
+
+  app.get('/info', (req, res) => {
+    res.json('luettelossa on ' + persons.length + ' henkilöä' + new Date())
+  })
+
+
+
 const PORT = 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
