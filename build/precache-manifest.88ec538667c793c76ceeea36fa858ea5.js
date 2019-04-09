ab1d650afafe058debb8365bdfc4c7e251499ede
Noora Virolainen <vino@cubbli18-gold.local>
2019-04-09 15:14:30 +0300

    frontend

---
+++ build/precache-manifest.88ec538667c793c76ceeea36fa858ea5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0384a941600a7897d171",
+    "url": "/static/js/main.1a7f539b.chunk.js"
+  },
+  {
+    "revision": "6dd00a54dcbb8e38ee27",
+    "url": "/static/js/2.afd81e94.chunk.js"
+  },
+  {
+    "revision": "0384a941600a7897d171",
+    "url": "/static/css/main.501958e5.chunk.css"
+  },
+  {
+    "revision": "501b2c9d7b0582338ac792d160714bdb",
+    "url": "/index.html"
+  }
+];
