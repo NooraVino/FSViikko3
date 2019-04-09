a74f5cb944f87943d1164aa899eca262a6d1146e
Noora Virolainen <vino@cubbli18-gold.local>
2019-04-09 13:42:48 +0300

    muutoksia

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ let persons = [
   }
 
 
-const PORT = 3001
-app.listen(PORT, () => {
-console.log(`Server running on port ${PORT}`)
-})
+  const PORT = 3000
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
